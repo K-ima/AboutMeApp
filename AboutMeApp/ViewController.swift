09f20499b7e21a5f393e63ac5685dc16c4b58ce7
K-ima <bulgin.a.v@bk.ru>
2023-08-05 13:31:11 +0300

    Finish first sqreen

--- AboutMeApp/ViewController.swift
+++ AboutMeApp/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Setting Outlets
+    @IBOutlet var textFieldName: UITextField!
+    @IBOutlet var textFieldPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
 
+    //MARK: - Setting Alerts
+    @IBAction func showAlertUserName() {
+        let alert = UIAlertController(
+            title: "Oops!",
+            message: "Your name is User",
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    @IBAction func showAlertPassword() {
+        let alert = UIAlertController(
+            title: "Oops!",
+            message: "Your password is 111",
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 
+    @IBAction func logInButton() {
+        if textFieldName.text != "User" || textFieldPassword.text != "111" {
+            showAlertLogIn()
+        }
+    }
+    
+    private func showAlertLogIn() {
+        let alert = UIAlertController(
+            title: "Invalid login or password",
+            message: "Please, enter correct login or password",
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
